0e55881a7765adc3e727323c35fb7ee187bd6502
wavy0 <69266903+wavy0@users.noreply.github.com>
2023-06-18 14:06:59 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,11 +1,11 @@
 $progressPreference = 'silentlyContinue'
 New-Item -Type Directory -Path $env:TEMP\Temp | Out-Null
-echo Downloading Xaml...
+echo "Downloading Xaml..."
 Invoke-WebRequest -Uri https://globalcdn.nuget.org/packages/microsoft.ui.xaml.2.7.0.nupkg -OutFile "$env:TEMP\temp\xaml.zip" -UseBasicParsing
 Expand-Archive -Path $env:TEMP\temp\xaml.zip -Force -DestinationPath $env:TEMP\temp\
 Add-AppxPackage -Path $env:TEMP\temp\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx
 Add-AppxPackage -Path $env:TEMP\temp\tools\AppX\x86\Release\Microsoft.UI.Xaml.2.7.appx
-echo Downloading winget...
+echo "Downloading winget..."
 $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
 $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
 Write-Information "Downloading winget to artifacts directory..."
