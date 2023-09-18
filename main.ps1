5b2dd88dd52fcb54da602a00e67725b2ad670733
wavy0 <69266903+wavy0@users.noreply.github.com>
2023-09-18 21:59:22 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -9,8 +9,8 @@ echo "Done."
 echo "Downloading winget..."
 $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
 $latestWingetMsixBundle = $latestWingetMsixBundleUri.Split("/")[-1]
-Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./$latestWingetMsixBundle"
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "$env:TEMP\$latestWingetMsixBundle"
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile $env:TEMP\temp\Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage $latestWingetMsixBundle
 Remove-Item -Path $env:TEMP\temp -Force -Recurse
